2dea9bfb525b8943edce42d5c166538ef7c894df
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2026-07-06 19:14:53 -0600

    style: Change application theme

--- app/src/main/java/com/example/wellnessdaily/ui/WellnessDailyScreen.kt
+++ app/src/main/java/com/example/wellnessdaily/ui/WellnessDailyScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -29,6 +28,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.wellnessdaily.R
 import com.example.wellnessdaily.data.DataSource
 import com.example.wellnessdaily.model.WellnessDay
